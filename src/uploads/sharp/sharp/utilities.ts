2c94323408c544a6c96fd858a351f909768877b8
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-03-19 13:55:56 +0530

    Create

---
+++ src/uploads/sharp/sharp/utilities.ts
@@ -0,0 +1,25 @@
+
+function format() {
+  return {};
+}
+function libvipsVersion() {
+  return '';
+}
+function cache(options) {
+  return {};
+}
+function cache(memory, files, items) {
+  return {};
+}
+function concurrency (concurrency) {
+  return 0;
+}
+function counters() {
+  return {};
+}
+function simd() {
+  return false;
+}
+function simd(simd) {
+  return simd;
+}
